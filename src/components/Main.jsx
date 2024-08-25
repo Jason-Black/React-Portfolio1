8245c2909ca7a698c33cedca7ce8fcd34b2ab7c3
Jason-Black <71369201+Jason-Black@users.noreply.github.com>
2024-08-25 23:26:24 +0000

    add the . to index

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -11,7 +11,7 @@ function Main() {
         <img className="w-full h-screen object-cover scale-x-[-1]" src={jsonImage}></img>
         <div className="w-full h-screen absolute top-0 left-0 bg-white/20">
         <div className='max-w-[700px] m-auto h-full w-full flex flex-col justify-center lg:items-start items-center'>
-            <h1 className='sm:text-5xl text-4xl font-bold text-gray-800'>I'm Jason Sutis</h1>
+            <h1 className='sm:text-5xl text-4xl font-bold text-gray-800'>I'm Jason Su</h1>
             <h2 className='flex sm:text-3xl text-2xl pt-4 text-gray-800'>I'm a <TypeAnimation
       sequence={[
         // Same substring at the start will only be typed out once, initially
