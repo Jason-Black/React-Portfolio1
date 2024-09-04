e3142c1ddde96e10ee0df39db9672e10dee7c329
Jason-Black <71369201+Jason-Black@users.noreply.github.com>
2024-09-04 17:53:12 -0400

    Update vite.config.js
    added /React-Portfolio1/ back to react config

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/',
+  base: '/React-Portfolio1/',
   plugins: [react()],
   test: {
     globals: true,
